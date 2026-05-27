19442076c64e8cc191fe6317801a6d27a0928cd8
Mauricio Fiorini <95772574+MauricioFiorini@users.noreply.github.com>
2026-05-27 07:52:35 -0300

    resolucion lab 3 cap 2

---
+++ Unidad.2.Extra.Lab.3.LINQ/ejerciciosresueltos/ClaseEmpleado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Empleado
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public decimal Sueldo { get; set; }
+    }
